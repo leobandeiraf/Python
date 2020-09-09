4d44b59668d01cc6ddbcb9c24dbfe222239d0d14
Leonardo Bandeira <leobandeiraf@gmail.com>
2020-09-08 23:53:06 -0300

    Delete 08.py

--- Solutions/08.py
+++
@@ -1,8 +0,0 @@
-#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês. 
-
-salarioHr = float(input())
-trabalhoHr = float(input())
-
-salario = salarioHr * trabalhoHr
-
-print(f"O salário bruto mensal é de R${round(salario, 2)}")
