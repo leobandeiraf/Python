9bae3ecd3ace5c923373dffb57cdd93cc67f274c
Bandeira <leobandeiraf@gmail.com>
2020-11-22 17:25:36 -0300

    Initial Commit

---
+++ (ARA0066-3602269) AV2/Calculadora Científica/app.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+import tkinter.font as tkFont
+
+
+class Calculadora:
+  def __init__(self, master):
+    super().__init__(master)
+
+
+root = Tk()
+app = Calculadora(root)
+app.mainloop()
